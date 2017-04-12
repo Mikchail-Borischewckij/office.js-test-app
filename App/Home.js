0fb120c7a875c2d0c9866f24a6197ee79804dc0e
mikbar <mik.borischewskij@gmail.com>
2017-04-12 13:42:51 +0300

    first

---
+++ App/Home.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+	var meetXUrl = "http://localhost:8080/officejs-app/App/Home.html";
+	Office.initialize = function (reason) {
+
+		window.location = meetXUrl;
+	};
+})();
