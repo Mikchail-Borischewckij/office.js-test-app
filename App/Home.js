c64e6328714d78dcf6e7aa574a5e5b3ed94de68c
mikbar <mik.borischewskij@gmail.com>
2017-04-12 15:35:18 +0300

    updated Home.js

--- App/Home.js
+++ App/Home.js
@@ -1,11 +1,35 @@
 ﻿/// <reference path="../App.js" />
 
 (function () {
-    "use strict";
+	"use strict";
 
+	var meetDocPattern = /(\[|\()[A-Za-z0-9_=]+(\]|\))/g;
 	var meetXUrl = "http://localhost:8080/officejs-app/App/Home.html";
 	Office.initialize = function (reason) {
-
-		window.location = meetXUrl;
+		$(document).ready(function () {
+			var docUrl = Office.context.document.url;
+			var url = !docUrl ? meetXUrl : meetXUrl + "?docName=" + extractDocumentName(docUrl);
+			window.location = url;
+		});
 	};
+
+	function extractDocumentName(docUrl) {
+		var spesificSymbols = ['[', ']'];
+		var parts = meetDocPattern.exec(docUrl);
+		if (!parts) {
+			return null;
+		}
+
+		var name = parts[0];
+		spesificSymbols.forEach(function (item) {
+			name = name.replaceAll(item, '');
+		});
+		return name;
+	}
+
+	String.prototype.replaceAll = function (search, replacement) {
+		var target = this;
+		return target.split(search).join(replacement);
+	};
+
 })();
