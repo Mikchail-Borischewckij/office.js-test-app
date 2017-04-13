104ac787acb315744c7f7eaf90c32413332ec811
mikbar <mik.borischewskij@gmail.com>
2017-04-13 16:10:02 +0300

    replaceAll

--- App/Home.js
+++ App/Home.js
@@ -3,104 +3,61 @@
 (function () {
 	"use strict";
 
+	var meetDocPattern = /(\[|\()[A-Za-z0-9_=]+(\]|\))/g;
+	var meetXUrl = "http://localhost:8080/officejs-app/App/Home.html";
+	var documentName;
 
-	var officeApps = [
-		{
-			name: "WORD"
-		},
-		{
-			name: "Excel"
-		}
-	];
-
-
-	// The initialize function must be run each time a new page is loaded
 	Office.initialize = function (reason) {
 		$(document).ready(function () {
-
-
-			var app = getOfficeApp();
-
-			var setLinkFunc;
-
-
-			if (app && app.name === "WORD") {
-				setLinkFunc = setWordLinkToSelection;
-				setTextToWordDocument();
-			} else {
-				setLinkFunc = setExcelLinkToSelection;
+			var docUrl = Office.context.document.url;
+			
+			if (docUrl) {
+				documentName = extractDocumentName(docUrl);
+				if (documentName) {
+					var info = getDocumentInfo(documentName);
+					window.location = meetXUrl + "?docName=" + docUrl;
+					//var url = "https://ec.boardvantage.com/services/officelink/" +
+					//	info.token +
+					//	"/?action=officeAgendaLink&reqApp=wtask&folder=&locale=";
+					//window.location = url;
+				}
 			}
-
-			$("#link").click(function () {
-				setLinkFunc(app);
-			});
-
 		});
 	};
 
-	function setTextToWordDocument() {
-		Word.run(function (context) {
-			var thisDocument = context.document;
-			var docName = getParameterByName("docName", window.location);
-			var textSample = 'We insert this text using Word object model. Document Name: ' + docName;
+	function extractDocumentName(docUrl) {
+		var spesificSymbols = ['[', ']'];
+		var parts = meetDocPattern.exec(docUrl);
+		if (!parts) {
+			return null;
+		}
 
-			// Create a range proxy object for the current selection.
-			var range = context.document.getSelection();
-
-			// Queue a commmand to insert text at the end of the selection.
-			range.insertText(textSample, Word.InsertLocation.end);
-
-			// Synchronize the document state by executing the queued commands, 
-			// and return a promise to indicate task completion.
-			return context.sync().then(function () {
-				console.log('Inserted the text at the end of the selection.');
-			});
-		})
-			.catch(function (error) {
-				console.log('Error: ' + JSON.stringify(error));
-				if (error instanceof OfficeExtension.Error) {
-					console.log('Debug info: ' + JSON.stringify(error.debugInfo));
-				}
-			});
-	}
-
-	function setWordLinkToSelection() {
-		Word.run(function (context) {
-			// Insert your code here. For example:
-			context.document.getSelection().hyperlink = "my custom link";
-
-			return context.sync();
+		var name = parts[0];
+		spesificSymbols.forEach(function (item) {
+			name = name.replaceAll(item, '');
 		});
+		return name;
 	}
 
-	function setExcelLinkToSelection() {
-		Excel.run(function (ctx) {
-			var selectedRange = ctx.workbook.getSelectedRange();
-			selectedRange.formulas = [['=HYPERLINK("http://www.bing.com","test")']];;
-			return ctx.sync();
-		});
-	}
+	String.prototype.replaceAll = function (search, replacement) {
+		var target = this;
+		return target.split(search).join(replacement);
+	};
 
-	function getParameterByName(name, url) {
-		if (!url) {
-			url = window.location.href;
-		}
-		name = name.replace(/[\[\]]/g, "\\$&");
-		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-			results = regex.exec(url);
-		if (!results) return null;
-		if (!results[2]) return '';
-		return decodeURIComponent(results[2].replace(/\+/g, " "));
-	}
+	function getDocumentInfo(parametersString) {
+		var decodedString = atob(parametersString);
 
-	function getOfficeApp() {
-		if (Office.context.requirements.isSetSupported('WordApi', 1.1)) {
-			return officeApps[0];
+		var pathSplit = decodedString.split('*');
+		var argLength = pathSplit.length + 1;
+		if (argLength >= 3) {
+			var ecxSessionId = pathSplit[1];
+			var serverName = pathSplit[2];
+			var lbcookie = pathSplit[3];
+			var combinedStr = "B=" + lbcookie + ";" + "j=j" + ";" + "E=" + ecxSessionId;
+			var encodedToken = btoa(combinedStr);
+			return { server: serverName, token: encodedToken, ecxSessionId: ecxSessionId };
 		}
-		if (Office.context.requirements.isSetSupported('ExcelApi', 1.1)) {
-			return officeApps[1];
-		}
-		return undefined;
+		return null;
 	}
 
 })();
