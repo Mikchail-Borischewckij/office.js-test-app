c84792ebaf697d345263437dcffa852c437c6f65
Mikhail Barycheuski <mik.borischewskij@gmail.com>
2017-07-05 17:33:51 +0300

    Update Home.js

--- App/Home.js
+++ App/Home.js
@@ -9,6 +9,7 @@
 
 	Office.initialize = function (reason) {
 		$(document).ready(function () {
+			Office.context.ui.displayDialogAsync('MeetXNotAvaliable.html');
 			var docUrl = Office.context.document.url;
 			documentName = extractDocumentName(docUrl);
 			window.location = meetXUrl + "?docName=" + documentName;
